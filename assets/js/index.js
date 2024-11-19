a969eb51180877c40f31b0f0b11c662c54808935
Ammarshahid123 <ammarpubgplayer97@gmail.com>
2024-11-20 02:56:45 +0500

    lul mera

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// JavaScript to handle the preloader and show the main content after 4 seconds
+window.onload = () => {
+    // Wait for 4 seconds before hiding the preloader and displaying the main content
+    setTimeout(() => {
+        document.body.classList.add('show-content');
+    }, 4000); // 4000ms = 4 seconds
+};
